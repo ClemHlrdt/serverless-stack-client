1bba4279df40c6595124db6360bdfba9312247fb
Clément Hélardot <clement.helardot@gmail.com>
2020-01-14 13:07:49 +0100

    Building our React app

--- src/Routes.js
+++ src/Routes.js
@@ -7,25 +7,39 @@ import AppliedRoute from "./components/AppliedRoute";
 import Signup from "./containers/Signup";
 import NewNote from "./containers/NewNote";
 import Notes from "./containers/Notes";
+import Settings from "./containers/Settings";
+import AuthenticatedRoute from "./components/AuthenticatedRoute";
+import UnauthenticatedRoute from "./components/UnauthenticatedRoute";
 
 export default function Routes({ appProps }) {
   return (
     <Switch>
       <AppliedRoute path="/" exact component={Home} appProps={appProps} />
-      <AppliedRoute path="/login" exact component={Login} appProps={appProps} />
-      <AppliedRoute
+      <UnauthenticatedRoute
+        path="/login"
+        exact
+        component={Login}
+        appProps={appProps}
+      />
+      <UnauthenticatedRoute
         path="/signup"
         exact
         component={Signup}
         appProps={appProps}
       />
-      <AppliedRoute
+      <AuthenticatedRoute
+        path="/settings"
+        exact
+        component={Settings}
+        appProps={appProps}
+      />
+      <AuthenticatedRoute
         path="/notes/new"
         exact
         component={NewNote}
         appProps={appProps}
       />
-      <AppliedRoute
+      <AuthenticatedRoute
         path="/notes/:id"
         exact
         component={Notes}
